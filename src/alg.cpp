fef73a4e3d0974f1280080919e019b8fa2f4c15a
ErrorDa <99288583+ErrorDa@users.noreply.github.com>
2022-04-09 23:41:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,23 @@
 // Copyright 2021 NNTU-CS
 #include <string>
 #include <map>
+#include <iostream>
 #include "tstack.h"
 
 int Prior(char pr) {
     switch (pr) {
     case '(':
       return 0;
-      break;
     case ')':
       return 1;
-      break;
     case '+': 
       return 2;
-      break;
     case '-': 
       return 2;
-      break;
     case '*': 
       return 3;
-      break;
     case '/': 
       return 3;
-      break;
     default:
         return -1;
     }
